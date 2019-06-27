e66c6584358b5a626956428d79face99d2216ccd
estmaia1 <estmaia@gmail.com>
2019-06-27 13:37:30 -0300

    update app.listen

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000, 'localhost', function() {
+app.listen(process.env.PORT, process.env.IP, function() {
 	console.log("The YelpCamp server has started!");
 });
